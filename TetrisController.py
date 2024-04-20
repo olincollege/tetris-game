af07abd55f1f72bd8cf0d7b1235efd47b5744fb9
Kaif Bailey <kdbailey2003@gmail.com>
2024-04-20 19:42:50 -0400

    Add controller class implementation

--- TetrisController.py
+++ TetrisController.py
@@ -1,7 +1,7 @@
 # importing all the required libraries
 import pygame
 from pygame.locals import *
-from sys import exit
+import sys
 
 # initiating pygame library to use it's
 # functions
@@ -16,16 +16,17 @@ class TetrisController:
 
     def control_piece(self):
 
-        if event.type == KEYDOWN:
-            if event.key == K_LEFT:
-                self.piece.move_left()
-            elif event.key == K_RIGHT:
-                self.piece.move_right()
-            elif event.key == K_UP:
-                self.piece.rotate_cw()
-            elif event.key == K_DOWN:
-                self.piece.rotate_ccw()
-            elif event.key == K_SPACE:
-                self.piece.drop()
-            elif event.key == K_z:
-                self.piece.fall_faster()
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_LEFT:
+                    self.piece.move_left()
+                elif event.key == K_RIGHT:
+                    self.piece.move_right()
+                elif event.key == K_UP:
+                    self.piece.rotate_cw()
+                elif event.key == K_DOWN:
+                    self.piece.rotate_ccw()
+                elif event.key == K_SPACE:
+                    self.piece.drop()
+                elif event.key == K_z:
+                    self.piece.fall_faster()
