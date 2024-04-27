f39b4962d7e68eef2cf5044b86b9022d885dc997
Paulo H <phitopoulos@olin.edu>
2024-04-27 19:11:38 -0400

    Fixed bugs, added methods.
    Added a method to the board class to add the
    relatively defined pieces to the board. Fixed a
    bug that affected all rows when one row was
    changed.

--- TetrisBoard.py
+++ TetrisBoard.py
@@ -1,17 +1,27 @@
 # Currently, this code only creates a list of lists and checks to see if
 # rows are full. It checks that by seeing if each pixel in a row is a space.
 # If at least one square is a space then it is not full.
-import TetrisListPiece
+# import TetrisListPiece
+# import TetrisRelativePieces
+import TetrisRelativePiecesCopy
+import random
 
 
 class TetrisBoard:
+    piece_types = [
+        TetrisRelativePiecesCopy.JPiece([3, 2]),
+        TetrisRelativePiecesCopy.LPiece([3, 2]),
+        TetrisRelativePiecesCopy.TPiece([3, 2]),
+    ]
+
     def __init__(self):
         rows = []
         for _ in range(10):
             rows.append(" ")
         board = []
         for _ in range(24):
-            board.append(rows)
+            board.append(rows[:])  # rows is sliced so that a change to one row
+            # ...only affects one row
         self._board = board
 
     def check_all_rows(self):
@@ -43,14 +53,31 @@ class TetrisBoard:
         self._board.insert(0, self._board[0])  ###This may make each row equal
         ###...to each other later on which could cause issues
 
-    def add_piece(self):
-        ActivePiece = TetrisListPiece.TetrisPiece()
-        print(ActivePiece.piece[0])
-        square_dim = len(ActivePiece.piece[0])
-        for i in range(square_dim):
-            base = [" " for j in range(10)]
-            base[3 : 3 + square_dim] = ActivePiece.piece[0][i]
-            self._board[i] = base
+    def add_rel_piece(self):
+        active_piece = random.choice(TetrisBoard.piece_types)
+        for i in active_piece.full_piece():
+            print(i)
+            print(self._board[i[1]][i[0]])
+            self._board[i[1]][i[0]] = "X"
+
+    #    def add_piece(self):
+    #        ActivePiece = random.choice(TetrisBoard.piece_types)
+
+    # def add_piece(self):
+    #        ActivePiece = TetrisListPiece.TetrisPiece()
+    #        square_dim = len(ActivePiece.piece[0])
+    #        for i in range(square_dim):
+    #            base = [" " for j in range(10)]
+    #            for
+    #            base[3 : 3 + square_dim] = (
+    #                ActivePiece.piece[0][i],
+    #                ActivePiece.color,
+    #                "active",
+    #            )
+    #            self._board[i] = base
+
+    # def check_fallen_piece(self):
+    # Should run before a piece falls a space
 
     def __repr__(self):
         return str(self._board)
